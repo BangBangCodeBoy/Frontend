251fcc91adf81a9a82b8076e892c0dcc6ebe678d
mingeung <jmk2229@naver.com>
2025-12-04 14:06:36 +0900

    feat: input 컴포넌트 생성

--- codeboy_frontend/src/components/Examples.vue
+++ codeboy_frontend/src/components/Examples.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Button from "@/components/ui/button/Button.vue";
+import Input from "@/components/ui/input/Input.vue";
 </script>
 
 <template>
@@ -9,6 +10,12 @@ import Button from "@/components/ui/button/Button.vue";
     <Button variant="outline">버튼4</Button>
     <Button variant="secondary">버튼5</Button>
   </div>
+  <div className="w-20">
+    <Label for="email">Email</Label>
+    <Input type="email" placeholder="email" />
+  </div>
+
+  <Input type="password" />
 </template>
 
 <style lang="scss" scoped></style>
