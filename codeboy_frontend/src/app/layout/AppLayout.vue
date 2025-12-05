f8042be16ecdfd4c1c8ca164cfd4c5d93845b0ba
mingeung <jmk2229@naver.com>
2025-12-05 13:17:12 +0900

    feat : 로그인 폼 UI 생성

--- codeboy_frontend/src/app/layout/AppLayout.vue
+++ codeboy_frontend/src/app/layout/AppLayout.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import Examples from "@/components/Examples.vue";
-import BottomNav from "@/shared/components/ui/BottomNav.vue";
-import Header from "@/shared/components/ui/Header.vue";
+import LoginForm from "@/features/auth/ui/LoginForm.vue";
+import AppHeader from "@/widgets/app-header/ui/AppHeader.vue";
+import BottomNav from "@/widgets/bottom-nav/ui/BottomNav.vue";
 </script>
 
 <template>
   <div>
-    <Header />
+    <AppHeader />
     <main className="m-auto w-full max-w-175 border-x px-4 py-6 ">
       <h1>레이아웃페이지</h1>
+      <LoginForm />
       <Examples />
 
       <RouterView />
---
+++ codeboy_frontend/src/features/auth/ui/LoginForm.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Input from "@/components/ui/input/Input.vue";
+import Button from "@/components/ui/button/Button.vue";
+</script>
+
+<template>
+  <div class="flex flex-col items-center px-6 pt-20">
+    <!-- title -->
+    <span class="Title2 mb-8">Login</span>
+
+    <!-- form area -->
+    <div class="flex flex-col gap-4 w-full max-w-sm mb-8">
+      <Input placeholder="ID" />
+      <Input placeholder="Password" type="password" />
+    </div>
+
+    <!-- actions -->
+    <div class="flex flex-col gap-3 w-full max-w-sm">
+      <Button variant="default">Login</Button>
+      <Button variant="outline">Sign in</Button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
