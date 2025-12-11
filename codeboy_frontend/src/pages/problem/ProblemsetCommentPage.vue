942d1ab8ac789715b3a738cb20199882033915e0
mingeung <jmk2229@naver.com>
2025-12-11 17:01:31 +0900

    feat : 오답노트 페이지 이동 버튼

--- codeboy_frontend/src/pages/problem/ProblemsetCommentPage.vue
+++ codeboy_frontend/src/pages/problem/ProblemsetCommentPage.vue
@@ -2,11 +2,12 @@
 import Button from "@/components/ui/button/Button.vue";
 import Input from "@/components/ui/input/Input.vue";
 import CommentItem from "@/entities/commnet/ui/CommentItem.vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { useCommentList } from "@/features/comment/model/useCommentList";
 import { onMounted } from "vue";
 
 const route = useRoute();
+const router = useRouter();
 
 const problemSetId = Number(route.params.id);
 
@@ -16,12 +17,16 @@ onMounted(() => {
   fetchCommentList(problemSetId);
 });
 
+const goIncorrectNote = () => {
+  router.push({ name: "incorrectNotes" });
+};
+
 console.log("화면에서 문제 리스트 출력해보기: ", commentList);
 </script>
 
 <template>
   <p>문제를 모두 풀었어요</p>
-  <Button>틀린 문제 복습하러 가기 </Button>
+  <Button @click="goIncorrectNote">틀린 문제 복습하러 가기 </Button>
 
   <div v-if="isLoading">댓글 불러오는 중...</div>
   <div v-else-if="error">{{ error.message }}</div>
